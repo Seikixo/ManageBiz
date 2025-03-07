9a93cffb62dfdf1204a455ce087f9caa15c1efb7
Seikixo <150346850+Seikixo@users.noreply.github.com>
2025-03-07 14:54:19 +0800

    update: Pages deleted

--- resources/js/Pages/Auth/Register.jsx
+++ resources/js/Pages/Auth/Register.jsx
@@ -1,7 +1,4 @@
 import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
-import PrimaryButton from '@/Components/PrimaryButton';
-import TextInput from '@/Components/TextInput';
 import GuestLayout from '@/Layouts/GuestLayout';
 import { Head, Link, useForm } from '@inertiajs/react';
 import { Button } from "@/Components/ui/button"
